2ae309d99b381d859148ef24ab955ac5dbc921b6
YgLK <kuba21316@wp.pl>
2022-12-04 21:13:38 +0100

    Add audience field into the voting creation form. Extend number of options to the range of 2 to 10.

--- pages/votings/create.js
+++ pages/votings/create.js
@@ -8,6 +8,13 @@ import { Router } from "../../routes";
 class VotingCreate extends Component {
   state = {
     topic: "",
+    /*
+      Audience:
+        0 - Students
+        1 - Employees
+        2 - All
+     */
+    audience: 0,
     options: ["", ""],
     optionsCount: 0,
     errorMessage: "",
@@ -21,7 +28,7 @@ class VotingCreate extends Component {
     try {
       const accounts = await web3.eth.getAccounts();
       await factory.methods
-        .createVoting(this.state.topic, this.state.options)
+        .createVoting(this.state.topic, this.state.options, this.state.audience)
         .send({
           from: accounts[0],
         });
@@ -32,6 +39,58 @@ class VotingCreate extends Component {
     }
     this.setState({ loading: false });
   };
+
+  createOptionField = (formDiv) => {
+    const optionCount = formDiv.querySelectorAll('input').length;
+    if(optionCount > 10){
+      window.alert('You cannot create more than 10 options per voting.');
+      return;
+    }
+
+    // create new option field
+    const optionLabel = document.createElement('label');
+    optionLabel.textContent = `Option ${optionCount}`;
+    const inputDiv = document.createElement('div');
+    inputDiv.classList.add('ui', 'input');
+    const inputField = document.createElement('input');
+    inputField.type = 'text';
+
+    // add slot for the option in state
+    const optionsCopy = this.state.options.slice();
+    optionsCopy.push("")
+    this.setState({options: optionsCopy});
+    // update options array on change
+    inputField.addEventListener('change', (event) => {
+      // create copy of options
+      const optionsCopy = this.state.options.slice();
+      optionsCopy[optionCount-1] = event.target.value;
+      this.setState({options: optionsCopy});
+    })
+
+    inputDiv.appendChild(inputField);
+    // add label and option to the form
+    formDiv.appendChild(optionLabel);
+    formDiv.appendChild(inputDiv);
+  }
+
+  removeOptionField = (formDiv) => {
+    const labels = formDiv.querySelectorAll('label');
+    const options = formDiv.querySelectorAll('.ui .input');
+    const  inputOptionsInputs = Array.from(options);
+
+    // remove topic input field from list
+    inputOptionsInputs.shift();
+    if(inputOptionsInputs.length  <= 2){
+      window.alert("At least two options must be provided");
+      return;
+    }
+
+    // remove option from state
+    this.setState({ options: this.state.options.slice(0, -1)});
+    // remove last option label and input field
+    labels[labels.length-1].remove();
+    options[options.length-1].remove();
+  }
 
   render() {
     return (
@@ -46,21 +105,35 @@ class VotingCreate extends Component {
                 this.setState({ topic: event.target.value })
               }
             />
+            <label>Audience</label>
+            <select onChange={(event) => {
+              this.setState({audience: event.target.value});
+            }}>
+              <option value="0">Students</option>
+              <option value="1">Employees</option>
+              <option value="2">All</option>
+          </select>
+            {/*// define options*/}
             <label>Option 1</label>
             <Input
               value={this.state.options[0]}
-              onChange={(event) =>
-                this.setState({ options: [event.target.value, this.state.options[1]] })
+              onChange={(event) => {
+                const optionsCopy = this.state.options.slice();
+                optionsCopy[0] = event.target.value;
+                this.setState({options: optionsCopy});
+                }
               }
             />
             <label>Option 2</label>
             <Input
               value={this.state.options[1]}
-              onChange={(event) =>
-                this.setState({ options: [this.state.options[0], event.target.value] })
+              onChange={(event) => {
+                let optionsCopy = this.state.options.slice();
+                optionsCopy[1] = event.target.value;
+                this.setState({options: optionsCopy});
               }
+            }
             />
-
           </Form.Field>
           <Message error header="Oops!" content={this.state.errorMessage} />
           
@@ -68,6 +141,25 @@ class VotingCreate extends Component {
             Create!
           </Button>
         </Form>
+        <Button onClick={() => {
+          let fieldsDiv = document.querySelector('.field');
+          this.createOptionField(fieldsDiv);
+        }}
+          variant="outlined"
+          icon="add"
+          content="Add option"
+          secondary
+        />
+        <Button onClick={() => {
+          let fieldsDiv = document.querySelector('.field');
+          this.removeOptionField(fieldsDiv);
+        }}
+          variant="outlined"
+          icon="minus"
+          content="Remove option"
+          secondary
+        />
+
       </Layout>
     );
   }
