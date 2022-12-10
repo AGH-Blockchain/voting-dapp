862dd5dedbed5f0fa18219a49a99d836d45d6a00
Maciek <maciejciepal@gmail.com>
2022-12-10 12:20:41 +0100

    Add VotingFactory tests

--- test/Voting.test.js
+++ test/Voting.test.js
@@ -12,50 +12,89 @@ let votingAddress;
 let voting;
 
 beforeEach(async () => {
-  accounts = await web3.eth.getAccounts();
+    accounts = await web3.eth.getAccounts();
 
-  factory = await new web3.eth.Contract(compiledFactory.abi)
-    .deploy({ data: compiledFactory.evm.bytecode.object })
-    .send({ from: accounts[0], gas: "10000000" });
+    factory = await new web3.eth.Contract(compiledFactory.abi)
+        .deploy({ data: compiledFactory.evm.bytecode.object })
+        .send({ from: accounts[0], gas: "10000000" });
 
-  await factory.methods.createVoting("Some topic", ["Option 1", "Option 2", "Option 3", "Option 4"]).send({
-    from: accounts[0],
-    gas: "10000000",
-  });
+    await factory.methods.createVoting("Some topic", ["Option 1", "Option 2", "Option 3", "Option 4"], 2).send({
+        from: accounts[0],
+        gas: "10000000",
+    });
 
-  [votingAddress] = await factory.methods.getDeployedVotings().call();
-  voting = await new web3.eth.Contract(compiledVoting.abi, votingAddress);
+    [votingAddress] = await factory.methods.getDeployedVotings().call();
+    voting = await new web3.eth.Contract(compiledVoting.abi, votingAddress);
 });
 
+describe("Voting Factory", () => {
+    it("add student", async () => {
+        await factory.methods.addStudent(accounts[1]).send({from: accounts[0]});
+        const isStudent = await factory.methods.students(accounts[1]).call();
+        assert.equal(isStudent, true);
+    })
+
+    it("add employee", async () => {
+        await factory.methods.addEmployee(accounts[1]).send({from: accounts[0]});
+        const isEmployee = await factory.methods.employees(accounts[1]).call();
+        assert.equal(isEmployee, true);
+    })
+
+    it("remove student", async () => {
+        var student = accounts[1];
+        await factory.methods.addStudent(student).send({from: accounts[0]});
+        await factory.methods.removeStudent(student).send({from: accounts[0]});
+        const isStudent = await factory.methods.isStudent(student).call();
+        assert.equal(isStudent, false);
+    })
+
+    it("remove employee", async () => {
+        var employee = accounts[1];
+        await factory.methods.addEmployee(employee).send({from: accounts[0]});
+        await factory.methods.removeEmployee(employee).send({from: accounts[0]});
+        const isStudent = await factory.methods.employees(employee).call();
+        assert.equal(isStudent, false);
+    })
+
+    it("restriction", async () => {
+        try {
+            await await factory.methods.addStudent(accounts[1]).send({from: accounts[1]});
+            assert.fail("The transaction should have thrown an error");
+        } catch(err) {
+            assert.equal(err.message, "VM Exception while processing transaction: revert Sender not authorized.");
+        }
+    })
+})
+
 describe("Votings", () => {
-  it("deploys a factory and a voting", () => {
-    assert.ok(factory.options.address);
-    assert.ok(voting.options.address);
-  });
-
-  it("marks caller as the voting creator", async () => {
-    const creator = await voting.methods.creator().call();
-    assert.equal(accounts[0], creator);
-  });
-
-  it("allows people to vote and marks them as voters", async () => {
-    await voting.methods.vote("Option 1").send({
-      from: accounts[1],
-      gas: "10000000"
+    it("deploys a factory and a voting", () => {
+        assert.ok(factory.options.address);
+        assert.ok(voting.options.address);
     });
-    const isVoter = await voting.methods.voters(accounts[1]).call();
-    assert(isVoter);
-  });
 
-  it("allows creator to close voting", async () => {
-    await voting.methods
-      .closeVoting()
-      .send({
-        from: accounts[0],
+    it("marks caller as the voting creator", async () => {
+        const creator = await voting.methods.creator().call();
+        assert.equal(accounts[0], creator);
+    });
+
+    it("allows people to vote and marks them as voters", async () => {
+        await voting.methods.vote("Option 1").send({
+        from: accounts[1],
         gas: "10000000"
-      });
-    const isClosed = await voting.methods.closed().call();
+        });
+        const isVoter = await voting.methods.voters(accounts[1]).call();
+        assert(isVoter);
+    });
 
-    assert(isClosed);
-  });
+    it("allows creator to close voting", async () => {
+        await voting.methods
+        .closeVoting()
+        .send({
+            from: accounts[0],
+            gas: "10000000"
+        });
+        const isClosed = await voting.methods.closed().call();
+
+        assert(isClosed);
+    });
 });
