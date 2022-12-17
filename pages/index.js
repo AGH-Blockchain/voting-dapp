70a4e6f0657d1672a418a534ee9e6bb606544e26
Marcin Retajczyk <45774493+RETAJD@users.noreply.github.com>
2022-12-17 22:21:35 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/votingFactory";
 import Layout from "../components/Layout";
 import { Link } from "../routes";
-import Voting from "../ethereum/voting";
 
 class VotingIndex extends Component {
   static async getInitialProps() {
@@ -14,9 +13,6 @@ class VotingIndex extends Component {
   renderVotings() {
     const items = this.props.votings.map((address) => {
       let voting = Voting(address);
-      voting.methods.getSummary().call().then((summary) => {
-        console.log(summary[1]);
-      });
 
       return {
         header: address,
