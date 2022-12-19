e92d4005a4d8a010e94c75a11577f3bc02d25a70
Michal Strzezon <mstrzezon@gmail.com>
2022-12-19 18:34:25 +0100

    # Changes - interaction with VotingFactory contract - simple README

--- offchainservice/src/main/java/pl/edu/agh/blockchain/offchainservice/service/VotingFactoryService.java
+++ offchainservice/src/main/java/pl/edu/agh/blockchain/offchainservice/service/VotingFactoryService.java
@@ -1,16 +1,41 @@
 package pl.edu.agh.blockchain.offchainservice.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.Web3jService;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.DefaultGasProvider;
 import pl.edu.agh.blockchain.offchainservice.contracts.VotingFactory;
 
 
 @Service
 public class VotingFactoryService {
+
+    @Value("${voting_factory.contract_address}")
+    private String contractAddress;
+
+    @Value("${voting_factory.service_url}")
+    private String serviceUrl;
+
+    @Value("${voting_factory.private_key}")
+    private String privateKey;
+
+    private final Web3jService service = new HttpService(serviceUrl);
+    private final Web3j web3j = Web3j.build(service);
+    private final Credentials credentials = Credentials.create(privateKey);
+    private final ContractGasProvider gasProvider = new DefaultGasProvider();
+
     public void addAddress(String email, String blockchainAddress) {
+
+        VotingFactory contract = VotingFactory.load(contractAddress, web3j, credentials, gasProvider);
+
         if (email.endsWith("@student.agh.edu.pl")) {
-            VotingFactory.addStudentAddress(blockchainAddress);
+            contract.addStudent(blockchainAddress);
         } else {
-            VotingFactory.addEmployeeAddress(blockchainAddress);
+            contract.addEmployee(blockchainAddress);
         }
     }
 }
